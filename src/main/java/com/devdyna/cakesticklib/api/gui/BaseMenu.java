98d20ad51471097414540d31ca78a8e5477e3020
DevDyna <andrearisorti2003@gmail.com>
2026-04-17 23:25:18 +0200

    ported basemenu and basescreen

---
+++ src/main/java/com/devdyna/cakesticklib/api/gui/BaseMenu.java
@@ -0,0 +1,246 @@
+package com.devdyna.cakesticklib.api.gui;
+
+import java.util.List;
+import java.util.function.Function;
+
+import com.devdyna.cakesticklib.api.aspect.logic.EnergyBlock;
+import com.devdyna.cakesticklib.api.aspect.logic.ItemStorageBlock;
+import com.devdyna.cakesticklib.api.aspect.logic.MachineItemAutomation;
+import com.devdyna.cakesticklib.setup.registry.types.zComponents;
+
+import net.minecraft.world.Container;
+import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.ContainerData;
+import net.minecraft.world.inventory.MenuType;
+import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.neoforged.neoforge.transfer.item.ItemStacksResourceHandler;
+import net.neoforged.neoforge.transfer.item.ResourceHandlerSlot;
+
+@SuppressWarnings("null")
+public abstract class BaseMenu extends AbstractContainerMenu {
+
+    public enum BlockMenuType {
+        CONTAINER,
+        MACHINE;
+    }
+
+    public int MACHINE_SLOT = 0;
+    public ContainerData energyData;
+    public List<Integer> inputSlotIndex;
+    public List<Integer> outputSlotIndex;
+    public BlockMenuType type;
+    private Container container;
+
+    protected BaseMenu(MenuType<?> menuType, int containerId, BlockEntity blockEntity) {
+        super(menuType, containerId);
+
+        if (blockEntity instanceof ItemStorageBlock be) {
+            this.type = BlockMenuType.CONTAINER;
+            this.MACHINE_SLOT = be.getSlots();
+            this.container = new SimpleContainer(MACHINE_SLOT);
+        }
+
+        if (blockEntity instanceof MachineItemAutomation be) {
+            this.type = BlockMenuType.MACHINE;
+            this.MACHINE_SLOT = be.getMachineSlots();
+            this.inputSlotIndex = be.getInputSlotIndex();
+            this.outputSlotIndex = be.getOutputSlotIndex();
+            this.container = new SimpleContainer(MACHINE_SLOT);
+        }
+
+        if (blockEntity instanceof EnergyBlock enel) {
+            energyData = enel.getContainerData();
+            addDataSlots(energyData);
+        }
+    }
+
+    protected void addPlayerSlots(Inventory inventory) {
+        addPlayerSlots(inventory, 0, 0);
+    }
+
+    protected void addPlayerSlots(Inventory inventory, int xf, int yf) {
+        addInventory(inventory, xf, yf);
+        addHotbar(inventory, xf, yf);
+    }
+
+    protected void addInventory(Inventory inventory, int xf, int yf) {
+        for (int i = 0; i < 3; ++i) {
+            for (int l = 0; l < 9; ++l) {
+                addSlot(new Slot(inventory, l + i * 9 + 9, (8 + l * 18) + xf, (84 + i * 18) + yf));
+            }
+        }
+    }
+
+    protected void addHotbar(Inventory inventory, int xf, int yf) {
+        for (int i = 0; i < 9; ++i) {
+            addSlot(new Slot(inventory, i, (8 + i * 18) + xf, 142 + yf));
+        }
+    }
+
+    protected void addInventory(Inventory inventory) {
+        addInventory(inventory, 0, 0);
+    }
+
+    protected void addHotbar(Inventory inventory) {
+        addHotbar(inventory, 0, 0);
+    }
+
+    protected void addMachineSlot(ItemStacksResourceHandler h, int index, int x, int y) {
+        addSlot(new ResourceHandlerSlot(h, h::set, index, x, y));
+    }
+
+    protected void addMachineOutputSlot(ItemStacksResourceHandler h, int id, int x, int y) {
+        addSlot(new ResourceHandlerSlot(h, h::set, id, x, y) {
+            @Override
+            public boolean mayPlace(ItemStack stack) {
+                return false;
+            }
+        });
+    }
+
+    protected void addMachineInputSlot(ItemStacksResourceHandler h, int id, int x, int y) {
+        addSlot(new ResourceHandlerSlot(h, h::set, id, x, y) {
+            @Override
+            public boolean mayPlace(ItemStack stack) {
+                return true;
+            }
+
+            @Override
+            public boolean mayPickup(Player playerIn) {
+                return true;
+            }
+        });
+    }
+
+    protected void addMachineSlot(Function<ItemStack, Boolean> mayPlace, ItemStacksResourceHandler h, int id, int limit,
+            int x,
+            int y) {
+        addSlot(new ResourceHandlerSlot(h, h::set, id, x, y) {
+            @Override
+            public boolean mayPlace(ItemStack stack) {
+                return mayPlace.apply(stack);
+            }
+
+            @Override
+            public boolean mayPickup(Player playerIn) {
+                return true;
+            }
+
+            @Override
+            public int getMaxStackSize() {
+                return limit;
+            }
+
+            @Override
+            public int getMaxStackSize(ItemStack i) {
+                return limit;
+            }
+        });
+    }
+
+    protected void addMachineUpgradeSlot(ItemStacksResourceHandler beSlot, int id, int x, int y) {
+        addMachineSlot(s -> s.has(zComponents.UPGRADE_COMPONENTS),
+                beSlot, id, 4, x, y);
+    }
+
+    // not work properly
+    @Override
+    public ItemStack quickMoveStack(Player player, int index) {
+        ItemStack movedStack = ItemStack.EMPTY;
+        Slot slot = this.slots.get(index);
+
+        if (slot != null && slot.hasItem()) {
+            ItemStack rawStack = slot.getItem();
+            movedStack = rawStack.copy();
+
+            if (outputSlotIndex != null && outputSlotIndex.contains(index)) {
+                if (!this.moveItemStackTo(rawStack, MACHINE_SLOT + 1, this.slots.size(), true)) {
+                    return ItemStack.EMPTY;
+                }
+                slot.onQuickCraft(rawStack, movedStack);
+            }
+
+            else if (index >= MACHINE_SLOT + 1) {
+                boolean movedToInput = false;
+
+                if (inputSlotIndex != null && !inputSlotIndex.isEmpty()) {
+                    for (int inputIndex : inputSlotIndex) {
+                        if (this.moveItemStackTo(rawStack, inputIndex, inputIndex + 1, false)) {
+                            movedToInput = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (!movedToInput) {
+                    int invStart = MACHINE_SLOT + 1;
+                    int invEnd = invStart + 27;
+                    int hotbarStart = invEnd;
+                    int hotbarEnd = this.slots.size();
+
+                    if (index >= invStart && index < invEnd) {
+                        if (!this.moveItemStackTo(rawStack, hotbarStart, hotbarEnd, false)) {
+                            return ItemStack.EMPTY;
+                        }
+                    } else if (index >= hotbarStart) {
+                        if (!this.moveItemStackTo(rawStack, invStart, invEnd, false)) {
+                            return ItemStack.EMPTY;
+                        }
+                    }
+                }
+            }
+
+            else if (inputSlotIndex != null && inputSlotIndex.contains(index)) {
+                if (!this.moveItemStackTo(rawStack, MACHINE_SLOT + 1, this.slots.size(), false)) {
+                    return ItemStack.EMPTY;
+                }
+            }
+
+            if (rawStack.isEmpty()) {
+                slot.setByPlayer(ItemStack.EMPTY);
+            } else {
+                slot.setChanged();
+            }
+
+            if (rawStack.getCount() == movedStack.getCount()) {
+                return ItemStack.EMPTY;
+            }
+
+            slot.onTake(player, rawStack);
+        }
+
+        return movedStack;
+    }
+
+    @Override
+    public boolean stillValid(Player player) {
+        return container.stillValid(player);
+    }
+
+    // @Override
+    // public boolean stillValid(Player player) {
+    // return ContainerLevelAccess.create(getLevel(),
+    // getBlockEntity().getBlockPos())
+    // .evaluate((lvl, pos) -> {
+    // for (Block b : getValidBlock()) {
+    // if (lvl.getBlockState(pos).is(b)) {
+    // return player.canInteractWithBlock(pos, 4.0);
+    // }
+    // }
+    // return false;
+    // }, true);
+    // }
+
+    // public abstract Block[] getValidBlock();
+
+    public abstract BlockEntity getBlockEntity();
+
+    public abstract Level getLevel();
+
+}
---
+++ src/main/java/com/devdyna/cakesticklib/api/gui/BaseScreen.java
@@ -0,0 +1,58 @@
+package com.devdyna.cakesticklib.api.gui;
+
+import java.awt.Color;
+
+import javax.annotation.Nullable;
+
+import com.devdyna.cakesticklib.api.utils.ColorUtil;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.GuiGraphicsExtractor;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.renderer.RenderPipelines;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.Identifier;
+import net.minecraft.util.FormattedCharSequence;
+import net.minecraft.world.entity.player.Inventory;
+
+@SuppressWarnings("null")
+public abstract class BaseScreen<T extends BaseMenu> extends AbstractContainerScreen<T> {
+
+    public BaseScreen(T menu, Inventory playerInventory, Component title) {
+        super(menu, playerInventory, title);
+    }
+
+    protected abstract Identifier background();
+
+    protected @Nullable Identifier arrow() {
+        return null;
+    }
+
+    protected boolean whenAnimateArrow() {
+        return false;
+    }
+
+    protected int getScaledArrowProgress() {
+        return 0;
+    }
+
+    protected final Color defaultToolTipColor = ColorUtil.color(64, 64, 64);
+
+
+    @Override
+    public void extractBackground(GuiGraphicsExtractor graphics, int mouseX, int mouseY, float a) {
+        graphics.blitSprite(RenderPipelines.GUI, background(), getGuiLeft(), getGuiTop(), 0, 0);
+        renderArrow(graphics);
+    }
+
+    protected void renderArrow(GuiGraphicsExtractor guiGraphics) {
+        if (arrow() != null && whenAnimateArrow())
+            guiGraphics.blit(arrow(), getGuiLeft() + 73, getGuiTop() + 35, 0, 0, getScaledArrowProgress(), 16, 24, 16);
+    }
+
+    public void drawCenteredString(GuiGraphicsExtractor pGuiGraphics, Font font, Component text, int x, int y,
+            int color, boolean dropShadow) {
+        FormattedCharSequence formattedcharsequence = text.getVisualOrderText();
+        pGuiGraphics.text(font, formattedcharsequence, x - font.width(formattedcharsequence) / 2, y, color, dropShadow);
+    }
+
+}
