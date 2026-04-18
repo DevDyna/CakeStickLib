7f7f6f6b4ccaae31ee6255164a2331c8f01dd959
DevDyna <andrearisorti2003@gmail.com>
2026-04-18 22:18:02 +0200

    fixed chisel recipe handling dont work anymore

--- src/main/java/com/devdyna/cakesticklib/setup/registry/builders/Chisel.java
+++ src/main/java/com/devdyna/cakesticklib/setup/registry/builders/Chisel.java
@@ -1,7 +1,5 @@
 package com.devdyna.cakesticklib.setup.registry.builders;
 
-import java.util.Optional;
-
 import com.devdyna.cakesticklib.api.utils.x;
 import com.devdyna.cakesticklib.setup.registry.types.zComponents;
 
@@ -11,10 +9,10 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.item.context.UseOnContext;
-import net.minecraft.world.item.crafting.RecipeHolder;
-import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.item.crafting.SelectableRecipe.SingleInputEntry;
 import net.minecraft.world.item.crafting.SingleRecipeInput;
 import net.minecraft.world.item.crafting.StonecutterRecipe;
 import net.minecraft.world.level.block.EntityBlock;
@@ -45,6 +43,10 @@ public class Chisel extends Item {
             level.playSound(player, pos, SoundEvents.RESPAWN_ANCHOR_CHARGE, SoundSource.BLOCKS, 1f, 1f);
             player.swing(hand);
         } else {
+
+            if (!item.has(zComponents.IDENTIFIER))
+                return InteractionResult.FAIL;
+
             var id = item.get(zComponents.IDENTIFIER);
 
             if (id == null)
@@ -52,45 +54,55 @@ public class Chisel extends Item {
 
             var saved = x.get(id);
 
-            Optional<RecipeHolder<StonecutterRecipe>> r = level.getServer().getRecipeManager()
-                    .getRecipeFor(RecipeType.STONECUTTING,
-                            new SingleRecipeInput(blockItem), level);
+            var recipes = level.recipeAccess().stonecutterRecipes().selectByInput(blockItem);
 
-            if (r == null || r.isEmpty())
+            if (recipes == null || recipes.isEmpty())
                 return InteractionResult.FAIL;
 
-            var recipe = r.get().value();
+            ItemStack result = null;
 
-            var result = recipe.assemble(new SingleRecipeInput(blockItem)).copy();
+            for (SingleInputEntry<StonecutterRecipe> entries : recipes.entries()) {
+                var r = entries.recipe().recipe();
 
-            if (result.is(saved)) {
+                if (r == null || r.isEmpty())
+                    continue;
 
-                if (saved instanceof BlockItem bi) {
-                    level.setBlockAndUpdate(pos, bi.getBlock().getStateForPlacement(new BlockPlaceContext(c)));
-                    if (bi.getBlock() instanceof EntityBlock) {
-                        level.getBlockEntity(pos).setChanged();
-                    }
-                } else {
-                    level.removeBlock(pos, false);
+                var output = r.get().value().assemble(new SingleRecipeInput(blockItem)).copy();
+
+                if (output.is(saved)) {
+                    result = output;
+                    break;
+                }
+            }
+
+            if (result == null)
+                return InteractionResult.FAIL;
+
+            if (saved instanceof BlockItem bi) {
+                level.setBlockAndUpdate(pos,
+                        bi.getBlock().getStateForPlacement(new BlockPlaceContext(c)));
+                if (bi.getBlock() instanceof EntityBlock) {
+                    level.getBlockEntity(pos).setChanged();
+                }
+            } else {
+                level.removeBlock(pos, false);
+                level.addFreshEntity(
+                        new ItemEntity(level, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5,
+                                x.item(saved)));
+            }
+
+            if (result.getCount() > 1) {
+                for (int j = 0; j < result.getCount() - 1; j++) {
                     level.addFreshEntity(
                             new ItemEntity(level, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5,
                                     x.item(saved)));
                 }
-
-                if (result.getCount() > 1) {
-                    for (int j = 0; j < result.getCount() - 1; j++) {
-                        level.addFreshEntity(
-                                new ItemEntity(level, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5,
-                                        x.item(saved)));
-                    }
-                }
-
-                player.swing(hand);
-                level.playSound(player, pos, SoundEvents.UI_STONECUTTER_TAKE_RESULT, SoundSource.BLOCKS, 1f, 1f);
-
-                return InteractionResult.SUCCESS;
             }
 
+            player.swing(hand);
+            level.playSound(player, pos, SoundEvents.UI_STONECUTTER_TAKE_RESULT, SoundSource.BLOCKS, 1f, 1f);
+
+            return InteractionResult.SUCCESS;
         }
         return InteractionResult.FAIL;
 
