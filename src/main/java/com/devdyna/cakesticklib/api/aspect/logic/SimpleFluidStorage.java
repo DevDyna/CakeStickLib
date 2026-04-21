3f79ee41e2c7b66d3ed36039718d28509e65388f
DevDyna <andrearisorti2003@gmail.com>
2026-04-21 15:06:03 +0200

    Update SimpleFluidStorage.java

--- src/main/java/com/devdyna/cakesticklib/api/aspect/logic/SimpleFluidStorage.java
+++ src/main/java/com/devdyna/cakesticklib/api/aspect/logic/SimpleFluidStorage.java
@@ -3,6 +3,7 @@ package com.devdyna.cakesticklib.api.aspect.logic;
 import com.devdyna.cakesticklib.api.utils.x;
 
 import net.minecraft.world.item.ItemStack;
+import net.neoforged.neoforge.fluids.FluidStack;
 import net.neoforged.neoforge.transfer.fluid.FluidStacksResourceHandler;
 
 public interface SimpleFluidStorage {
@@ -14,7 +15,11 @@ public interface SimpleFluidStorage {
     int getTanks();
 
     default ItemStack getAsBucket(int index) {
-        return x.item(getFluidStorage().getResource(getTankCapacity()).getFluid().getBucket());
+        return x.item(getFluidStorage().getResource(index).getFluid().getBucket());
+    }
+
+    default FluidStack getAsStack(int index) {
+        return getFluidStorage().getResource(index).toStack(getFluidStorage().getAmountAsInt(index));
     }
 
 }
