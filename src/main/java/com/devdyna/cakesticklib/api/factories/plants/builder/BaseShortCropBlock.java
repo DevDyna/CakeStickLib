0c5a774b973489691f94a725f5ff9e75f926ae45
DevDyna <andrearisorti2003@gmail.com>
2026-06-19 02:05:53 +0200

    Update BaseShortCropBlock.java

--- src/main/java/com/devdyna/cakesticklib/api/factories/plants/builder/BaseShortCropBlock.java
+++ src/main/java/com/devdyna/cakesticklib/api/factories/plants/builder/BaseShortCropBlock.java
@@ -29,7 +29,7 @@ public class BaseShortCropBlock extends BaseCropBlock {
     }
 
     @Override
-    protected IntegerProperty getAgeProperty() {
+    public IntegerProperty getAgeProperty() {
         return AGE;
     }
 
