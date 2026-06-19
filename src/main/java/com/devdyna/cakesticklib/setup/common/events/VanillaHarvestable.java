7730c8f89a7e7c82e7a313a9c254669cf07924ea
DevDyna <andrearisorti2003@gmail.com>
2026-06-19 21:05:15 +0200

    fixed bonemeal interaction with harvestable cause conflicts

--- src/main/java/com/devdyna/cakesticklib/setup/common/events/VanillaHarvestable.java
+++ src/main/java/com/devdyna/cakesticklib/setup/common/events/VanillaHarvestable.java
@@ -5,6 +5,8 @@ import java.util.*;
 import com.devdyna.cakesticklib.api.factories.plants.*;
 
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.item.BoneMealItem;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
@@ -16,16 +18,25 @@ public class VanillaHarvestable {
         var level = event.getLevel();
         var player = event.getEntity();
         var hand = event.getHand();
+        var item = event.getItemStack();
 
         if (!Harvestable.getConfig())
             return;
 
-        List<ItemStack> check = VanillaPlants.checkReplant(level, pos,player,hand);
+        if (player.isSpectator())
+            return;
 
-        if (check != null && hand.equals(InteractionHand.MAIN_HAND)) 
-            check.forEach(i -> player.addItem(i));
-        
 
-      
+            //prevent bonemeal usage conflict
+        if (!(item.getItem() instanceof BoneMealItem) && hand.equals(InteractionHand.MAIN_HAND)) {
+            List<ItemStack> check = VanillaPlants.checkReplant(level, pos, player, hand);
+
+            if (check != null) {
+                check.forEach(i -> player.addItem(i));
+                event.setCanceled(true);
+                event.setCancellationResult(InteractionResult.SUCCESS);
+            }
+        }
+
     }
 }
