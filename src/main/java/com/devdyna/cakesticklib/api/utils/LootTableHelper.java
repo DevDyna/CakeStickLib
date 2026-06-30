9c3fd3dbf09d0fbfd1f150bff59643dfd2a3c2a1
DevDyna <andrearisorti2003@gmail.com>
2026-06-30 17:36:50 +0200

    Update LootTableHelper.java

--- src/main/java/com/devdyna/cakesticklib/api/utils/LootTableHelper.java
+++ src/main/java/com/devdyna/cakesticklib/api/utils/LootTableHelper.java
@@ -3,16 +3,25 @@ package com.devdyna.cakesticklib.api.utils;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.Nullable;
+
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.storage.loot.LootParams;
 import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
+import net.minecraft.world.phys.Vec3;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
@@ -56,6 +65,26 @@ public class LootTableHelper {
     public static List<ItemStack> getItemStackFromLootTable(ServerLevel level, ResourceKey<LootTable> rk,
             LootParams p) {
         return getLootTable(level, rk).getRandomItems(p);
+    }
+
+    public static List<ItemStack> getItemsStackWithParams(BlockState state, ServerLevel server, BlockPos pos,
+            ItemStack tool, @Nullable Player player, @Nullable BlockEntity be) {
+        return state.getDrops(new LootParams.Builder(server)
+                .withParameter(LootContextParams.ORIGIN, Vec3.atCenterOf(pos))
+                .withParameter(LootContextParams.BLOCK_STATE, state)
+                .withParameter(LootContextParams.TOOL, tool)
+                .withOptionalParameter(LootContextParams.THIS_ENTITY, player)
+                .withOptionalParameter(LootContextParams.BLOCK_ENTITY, be));
+    }
+
+    public static List<ItemStack> getAsSilkTouch(BlockState state, ServerLevel server, BlockPos pos,
+            ItemStack tool, @Nullable Player player, @Nullable BlockEntity be) {
+
+        ItemStack copy = tool.copy();
+        copy.enchant(EnchantUtil.getEnchantHolder(server, Enchantments.SILK_TOUCH), 1);
+
+        return getItemsStackWithParams(state, server, pos, copy, player, be);
+
     }
 
 }
