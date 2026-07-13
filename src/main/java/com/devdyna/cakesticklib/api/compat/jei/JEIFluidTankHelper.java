42ab12af1778c95cd689a7c5d9f7bd577f1c392f
DevDyna <andrearisorti2003@gmail.com>
2026-07-13 16:28:39 +0200

    Update JEIFluidTankHelper.java

--- src/main/java/com/devdyna/cakesticklib/api/compat/jei/JEIFluidTankHelper.java
+++ src/main/java/com/devdyna/cakesticklib/api/compat/jei/JEIFluidTankHelper.java
@@ -47,9 +47,17 @@ public class JEIFluidTankHelper {
         return new JEIFluidTankHelper();
     }
 
-    public void build(BiFunction<Integer, Integer, IRecipeSlotBuilder> builder) {
+    @Deprecated
+    public void oldbuild(BiFunction<Integer, Integer, IRecipeSlotBuilder> builder) {
         var height = (int) (Math.min(16, Math.max((int) ((fluids.getFirst().getAmount() + 256) * 0.016), 1)) * h);
-        builder.apply((int) (x0 * w), y0 - height)//TODO +16
+        builder.apply((int) (x0 * w), y0 - height)
+                .addIngredients(NeoForgeTypes.FLUID_STACK, fluids)
+                .setFluidRenderer(fluids.getFirst().getAmount(), false, (int) (w * 16), height);
+    }
+
+    public void build(BiFunction<Integer, Integer, IRecipeSlotBuilder> builder) {
+        var height = (int) (Math.min(16, Math.max((int) ((fluids.getFirst().getAmount() + 256) * 0.016), 1)) * h)+16;
+        builder.apply((int) (x0 * w), y0 - height)
                 .addIngredients(NeoForgeTypes.FLUID_STACK, fluids)
                 .setFluidRenderer(fluids.getFirst().getAmount(), false, (int) (w * 16), height);
     }
