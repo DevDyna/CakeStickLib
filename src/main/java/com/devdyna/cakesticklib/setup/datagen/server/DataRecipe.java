457a72364c1f481f12bff1b89dce2b3180bd6adc
DevDyna <andrearisorti2003@gmail.com>
2026-08-27 20:48:42 +0200

    thousand of new recipes and itemtags related

--- src/main/java/com/devdyna/cakesticklib/setup/datagen/server/DataRecipe.java
+++ src/main/java/com/devdyna/cakesticklib/setup/datagen/server/DataRecipe.java
@@ -2,9 +2,12 @@ package com.devdyna.cakesticklib.setup.datagen.server;
 
 import static com.devdyna.cakesticklib.CakeStickLib.MODULE_ID;
 
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 import com.devdyna.cakesticklib.api.datagen.RecipeGenerators;
+import com.devdyna.cakesticklib.api.utils.x;
 import com.devdyna.cakesticklib.setup.common.recipes.item_replace.ReplaceItemBuilder;
 import com.devdyna.cakesticklib.setup.common.recipes.oxidation.CopperOxidationBuilder;
 import com.devdyna.cakesticklib.setup.common.recipes.oxidation.OxidationStatus;
@@ -366,24 +369,228 @@ public class DataRecipe extends RecipeProvider implements RecipeGenerators {
                                 .unlockedBy()
                                 .save(output);
 
-                ReplaceItemBuilder.of(registries)
-                                .replace(Items.ACACIA_CHEST_BOAT, Items.ACACIA_BOAT)
-                                .output(Items.CHEST)
+                DurabilityConsumeBuilder.of(registries)
+                                .tool(Items.SHEARS, 4)
+                                .add(ItemTags.WOOL)
+                                .output(Items.STRING, 4)
+                                .unlockedBy()
+                                .save(output, "_from_wool");
+
+                DurabilityConsumeBuilder.of(registries)
+                                .tool(Items.SHEARS, 3)
+                                .add(ItemTags.WOOL_CARPETS)
+                                .output(Items.STRING, 2)
+                                .unlockedBy()
+                                .save(output, "_from_wool_carpets");
+
+                DurabilityConsumeBuilder.of(registries)
+                                .tool(Items.SHEARS, 12)
+                                .add(Items.DRIED_GHAST)
+                                .output(Items.GHAST_TEAR, 4)
+                                .unlockedBy()
+                                .save(output);
+
+                DurabilityConsumeBuilder.of(registries)
+                                .tool(LibItems.REDSTONE_ACID.get(), 1)
+                                .add(ItemTags.CANDLES)
+                                .output(Items.CANDLE, 1)
+                                .unlockedBy()
+                                .save(output);
+
+                DurabilityConsumeBuilder.of(registries)
+                                .tool(LibItems.REDSTONE_ACID.get(), 1)
+                                .add(ItemTags.TERRACOTTA)
+                                .output(Items.TERRACOTTA, 1)
+                                .unlockedBy()
+                                .save(output);
+
+                DurabilityConsumeBuilder.of(registries)
+                                .tool(LibItems.REDSTONE_ACID.get(), 1)
+                                .add(Tags.Items.GLASS_BLOCKS_CHEAP)
+                                .output(Items.GLASS, 1)
+                                .unlockedBy()
+                                .save(output);
+
+                DurabilityConsumeBuilder.of(registries)
+                                .tool(LibItems.REDSTONE_ACID.get(), 1)
+                                .add(Tags.Items.GLASS_PANES)
+                                .output(Items.GLASS_PANE, 1)
+                                .unlockedBy()
+                                .save(output);
+
+                DurabilityConsumeBuilder.of(registries)
+                                .tool(ItemTags.SHOVELS, 10)
+                                .add(Tags.Items.GRAVELS)
+                                .output(Items.FLINT, 1)
+                                .unlockedBy()
+                                .save(output);
+
+                DurabilityConsumeBuilder.of(registries)
+                                .tool(ItemTags.PICKAXES, 10)
+                                .add(Items.AMETHYST_BLOCK)
+                                .output(Items.AMETHYST_SHARD, 4)
+                                .unlockedBy()
+                                .save(output);
+
+                Map.ofEntries(
+                                Map.entry(Items.OAK_LOG, Items.STRIPPED_OAK_LOG),
+                                Map.entry(Items.SPRUCE_LOG, Items.STRIPPED_SPRUCE_LOG),
+                                Map.entry(Items.BIRCH_LOG, Items.STRIPPED_BIRCH_LOG),
+                                Map.entry(Items.JUNGLE_LOG, Items.STRIPPED_JUNGLE_LOG),
+                                Map.entry(Items.ACACIA_LOG, Items.STRIPPED_ACACIA_LOG),
+                                Map.entry(Items.DARK_OAK_LOG, Items.STRIPPED_DARK_OAK_LOG),
+                                Map.entry(Items.MANGROVE_LOG, Items.STRIPPED_MANGROVE_LOG),
+                                Map.entry(Items.CHERRY_LOG, Items.STRIPPED_CHERRY_LOG),
+                                Map.entry(Items.PALE_OAK_LOG, Items.STRIPPED_PALE_OAK_LOG),
+                                Map.entry(Items.BAMBOO_BLOCK, Items.STRIPPED_BAMBOO_BLOCK),
+
+                                Map.entry(Items.OAK_WOOD, Items.STRIPPED_OAK_WOOD),
+                                Map.entry(Items.SPRUCE_WOOD, Items.STRIPPED_SPRUCE_WOOD),
+                                Map.entry(Items.BIRCH_WOOD, Items.STRIPPED_BIRCH_WOOD),
+                                Map.entry(Items.JUNGLE_WOOD, Items.STRIPPED_JUNGLE_WOOD),
+                                Map.entry(Items.ACACIA_WOOD, Items.STRIPPED_ACACIA_WOOD),
+                                Map.entry(Items.DARK_OAK_WOOD, Items.STRIPPED_DARK_OAK_WOOD),
+                                Map.entry(Items.MANGROVE_WOOD, Items.STRIPPED_MANGROVE_WOOD),
+                                Map.entry(Items.CHERRY_WOOD, Items.STRIPPED_CHERRY_WOOD),
+                                Map.entry(Items.PALE_OAK_WOOD, Items.STRIPPED_PALE_OAK_WOOD),
+
+                                Map.entry(Items.CRIMSON_STEM, Items.STRIPPED_CRIMSON_STEM),
+                                Map.entry(Items.CRIMSON_HYPHAE, Items.STRIPPED_CRIMSON_HYPHAE),
+                                Map.entry(Items.WARPED_STEM, Items.STRIPPED_WARPED_STEM),
+                                Map.entry(Items.WARPED_HYPHAE, Items.STRIPPED_WARPED_HYPHAE))
+                                .forEach((log, stripped) -> DurabilityConsumeBuilder.of(registries)
+                                                .tool(ItemTags.AXES, 1)
+                                                .add(log)
+                                                .output(stripped)
+                                                .unlockedBy()
+                                                .save(output));
+
+                DurabilityConsumeBuilder.of(registries)
+                                .tool(ItemTags.HOES, 1)
+                                .add(LibTags.Items.TURN_INTO_FARMLAND)
+                                .output(Items.FARMLAND)
+                                .unlockedBy()
+                                .save(output);
+
+                DurabilityConsumeBuilder.of(registries)
+                                .tool(ItemTags.SHOVELS, 1)
+                                .add(LibTags.Items.TURN_INTO_DIRT_PATH)
+                                .output(Items.DIRT_PATH)
+                                .unlockedBy()
+                                .save(output);
+
+                DurabilityConsumeBuilder.of(registries)
+                                .tool(ItemTags.HOES, 1)
+                                .add(LibTags.Items.TURN_INTO_DIRT)
+                                .output(Items.DIRT)
                                 .unlockedBy()
                                 .save(output);
 
                 ReplaceItemBuilder.of(registries)
-                                .add(Items.AMETHYST_SHARD)
-                                .replace(Items.SMALL_AMETHYST_BUD, Items.MEDIUM_AMETHYST_BUD)
-                                .output(Items.GOLD_NUGGET)
+                                .replace(Items.JACK_O_LANTERN, Items.TORCH)
+                                .output(Items.CARVED_PUMPKIN)
                                 .unlockedBy()
                                 .save(output);
 
                 ReplaceItemBuilder.of(registries)
-                                .add(Items.AMETHYST_SHARD)
-                                .replace(Items.MEDIUM_AMETHYST_BUD, Items.LARGE_AMETHYST_BUD)
-                                .output(Items.GOLD_INGOT)
+                                .replace(Items.CAMPFIRE, Items.CHARCOAL)
+                                .add(ItemTags.SOUL_FIRE_BASE_BLOCKS)
+                                .output(Items.SOUL_CAMPFIRE)
                                 .unlockedBy()
+                                .save(output);
+
+                ReplaceItemBuilder.of(registries)
+                                .replace(Items.SOUL_CAMPFIRE, Items.SOUL_SOIL)
+                                .add(ItemTags.COALS)
+                                .output(Items.CAMPFIRE)
+                                .unlockedBy()
+                                .save(output);
+
+                Map.of(
+                                Items.OAK_CHEST_BOAT, Items.OAK_BOAT,
+                                Items.BIRCH_CHEST_BOAT, Items.BIRCH_BOAT,
+                                Items.SPRUCE_CHEST_BOAT, Items.SPRUCE_BOAT,
+                                Items.JUNGLE_CHEST_BOAT, Items.JUNGLE_BOAT,
+                                Items.ACACIA_CHEST_BOAT, Items.ACACIA_BOAT,
+                                Items.DARK_OAK_CHEST_BOAT, Items.DARK_OAK_BOAT,
+                                Items.MANGROVE_CHEST_BOAT, Items.MANGROVE_BOAT,
+                                Items.CHERRY_CHEST_BOAT, Items.CHERRY_BOAT,
+                                Items.BAMBOO_CHEST_RAFT, Items.BAMBOO_RAFT,
+                                Items.PALE_OAK_CHEST_BOAT, Items.PALE_OAK_BOAT)
+                                .forEach((chest, boat) -> ReplaceItemBuilder.of(registries)
+                                                .replace(chest, boat)
+                                                .output(Items.CHEST)
+                                                .unlockedBy()
+                                                .save(output, "_from_" + x.name(chest)));
+
+                Map.of(
+                                Items.CHEST_MINECART, Items.CHEST,
+                                Items.TNT_MINECART, Items.TNT,
+                                Items.FURNACE_MINECART, Items.FURNACE,
+                                Items.HOPPER_MINECART, Items.HOPPER)
+                                .forEach((cart, result) -> ReplaceItemBuilder.of(registries)
+                                                .replace(cart, Items.MINECART)
+                                                .output(result)
+                                                .unlockedBy()
+                                                .save(output));
+
+                List.of(
+                                Items.ANGLER_POTTERY_SHERD,
+                                Items.ARCHER_POTTERY_SHERD,
+                                Items.ARMS_UP_POTTERY_SHERD,
+                                Items.BLADE_POTTERY_SHERD,
+                                Items.BREWER_POTTERY_SHERD,
+                                Items.BURN_POTTERY_SHERD,
+                                Items.DANGER_POTTERY_SHERD,
+                                Items.EXPLORER_POTTERY_SHERD,
+                                Items.FLOW_POTTERY_SHERD,
+                                Items.FRIEND_POTTERY_SHERD,
+                                Items.GUSTER_POTTERY_SHERD,
+                                Items.HEART_POTTERY_SHERD,
+                                Items.HEARTBREAK_POTTERY_SHERD,
+                                Items.HOWL_POTTERY_SHERD,
+                                Items.MINER_POTTERY_SHERD,
+                                Items.MOURNER_POTTERY_SHERD,
+                                Items.PLENTY_POTTERY_SHERD,
+                                Items.PRIZE_POTTERY_SHERD,
+                                Items.SCRAPE_POTTERY_SHERD,
+                                Items.SHEAF_POTTERY_SHERD,
+                                Items.SHELTER_POTTERY_SHERD,
+                                Items.SKULL_POTTERY_SHERD,
+                                Items.SNORT_POTTERY_SHERD)
+                                .forEach(sherd -> ReplaceItemBuilder.of(registries)
+                                                .replace(sherd, sherd)
+                                                .add(Items.BRICK)
+                                                .add(LibItems.PATINA)
+                                                .output(sherd)
+                                                .unlockedBy()
+                                                .save(output));
+
+                Map.of(
+                                Items.COD_BUCKET, Items.COD,
+                                Items.SALMON_BUCKET, Items.SALMON,
+                                Items.PUFFERFISH_BUCKET, Items.PUFFERFISH,
+                                Items.TROPICAL_FISH_BUCKET, Items.TROPICAL_FISH)
+                                .forEach((bucket, fish) -> ReplaceItemBuilder.of(registries)
+                                                .replace(bucket, Items.WATER_BUCKET)
+                                                .output(fish)
+                                                .unlockedBy()
+                                                .save(output));
+
+                ReplaceItemBuilder.of(registries)
+                                .replace(Items.GLOW_ITEM_FRAME, Items.GLOW_INK_SAC)
+                                .output(Items.ITEM_FRAME)
+                                .unlockedBy()
+                                .save(output);
+
+                // shapeless(RecipeCategory.MISC, Items.BOOK)
+                //                 .requires(LibTags.Items.WRITABLE_BOOKS)
+                //                 .unlockedBy(getHasName(LibTags.Items.WRITABLE_BOOKS), has(LibTags.Items.WRITABLE_BOOKS))
+                //                 .save(output);
+
+                shapeless(RecipeCategory.MISC, Items.CLAY_BALL, 4)
+                                .requires(Items.CLAY)
+                                .unlockedBy(getHasName(Items.CLAY), has(Items.CLAY))
                                 .save(output);
 
                 plate(output, LibTags.Items.STEEL_INGOT, LibItems.STEEL_PLATE.get());
