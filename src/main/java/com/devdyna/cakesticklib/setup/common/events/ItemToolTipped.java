8dd04e4e54b728596dabde75ad8f65bd9fde4546
DevDyna <andrearisorti2003@gmail.com>
2026-09-15 17:38:50 +0200

    changed type part of tooltip to be more clean

--- src/main/java/com/devdyna/cakesticklib/setup/common/events/ItemToolTipped.java
+++ src/main/java/com/devdyna/cakesticklib/setup/common/events/ItemToolTipped.java
@@ -11,7 +11,6 @@ import com.devdyna.cakesticklib.api.upgrades.modifiers.DirectionalModifier;
 import com.devdyna.cakesticklib.api.upgrades.modifiers.ModifierUtils;
 import com.devdyna.cakesticklib.api.upgrades.modifiers.NumericModifier;
 import com.devdyna.cakesticklib.api.upgrades.modifiers.base.BaseModifier.UseType;
-import com.devdyna.cakesticklib.api.utils.StringUtil;
 import com.devdyna.cakesticklib.api.utils.x;
 import com.devdyna.cakesticklib.setup.registry.builders.CakeStick;
 import com.devdyna.cakesticklib.setup.registry.builders.Chisel;
@@ -69,10 +68,12 @@ public class ItemToolTipped {
                     tip.add(2, Component.translatable(MODULE_ID + ".upgrades.modifier.eject.shift"));
 
                     var eject = ((DirectionalModifier) ModifierUtils.get(nbt, UpgradeType.EJECT));
+
                     tip.add(2, Component.translatable(MODULE_ID + ".upgrades.modifier.eject",
                             TipColors.GOLD + eject.dir().name(),
-                            (eject.type() == UseType.FLUID ? TipColors.LIGHT_BLUE : TipColors.GREEN)
-                                    + StringUtil.nameCapitalized(eject.type().getId())));
+                            (eject.type() == UseType.FLUID ? TipColors.LIGHT_BLUE : TipColors.YELLOW)
+                                    + eject.type().name()));
+
                 }
 
             }
